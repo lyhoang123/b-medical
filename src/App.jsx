4eff326aad76cb828b64d495493840fdbb9d6c27
Van Khoa <lyhoang20012000@gmail.com>
2022-03-28 16:37:11 +0700

    update form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { Web3Provider } from "@ethersproject/providers";
-import { useWeb3React, Web3ReactProvider } from "@web3-react/core";
-import { Routes } from "components/route/Routes";
-import { useEagerConnect, useInactiveListener } from "connectors/hooks";
-import AdminPage from "pages/AdminPage";
-import NFTDetail from "pages/NFTDetail";
-import ProductField from "pages/ProductField";
-import { useEffect, useState } from "react";
+import { Web3Provider } from '@ethersproject/providers';
+import { useWeb3React, Web3ReactProvider } from '@web3-react/core';
+import { Routes } from 'components/route/Routes';
+import { useEagerConnect, useInactiveListener } from 'connectors/hooks';
+import { useEffect, useState } from 'react';
 
 function getLibrary(provider) {
   const library = new Web3Provider(provider);
@@ -33,7 +30,6 @@ export default function () {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
       <App />
-      {/* <ProductField /> */}
     </Web3ReactProvider>
   );
 }
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,89 +1,89 @@
-import { Box, Button, Text } from "@chakra-ui/react";
-import React from "react";
+import { Box, Button, Text } from '@chakra-ui/react';
+import React from 'react';
 import {
   AiFillCheckCircle,
   AiFillCloseCircle,
   AiOutlineFileSearch,
-} from "react-icons/ai";
-import "../styles/AdminPage.css";
+} from 'react-icons/ai';
+import '../styles/AdminPage.css';
 
 AdminPage.propTypes = {};
 
 const CompanyList = () => {
   return (
-    <Box className="body__box">
-      <Box className="box__body-left">
-        <Box className="box__content-header">
-          <Text className="content__title">Tên doanh nghiệp </Text>
+    <Box className='body__box'>
+      <Box className='box__body-left'>
+        <Box className='box__content-header'>
+          <Text className='content__title'>Tên doanh nghiệp </Text>
           <span>:</span>
           <Text>CÔNG TY TNHH THIẾT BỊ KỸ THUẬT Y KHOA VIỆT MỸ </Text>
         </Box>
-        <Box className="main__content">
+        <Box className='main__content'>
           <Box>
-            <Box className="content">
-              <Text className="content__title">Tỉnh / Thành </Text>
+            <Box className='content'>
+              <Text className='content__title'>Tỉnh / Thành </Text>
               <span>:</span>
               <Text>Thành Phố Hồ Chí Minh</Text>
             </Box>
-            <Box className="content">
-              <Text className="content__title">Quận / Huyện </Text>
+            <Box className='content'>
+              <Text className='content__title'>Quận / Huyện </Text>
               <span>:</span>
               <Text>Gò Vấp</Text>
             </Box>
-            <Box className="content">
-              <Text className="content__title">Phường / Xã </Text>
+            <Box className='content'>
+              <Text className='content__title'>Phường / Xã </Text>
               <span>:</span>
               <Text>Phường 1</Text>
             </Box>
-            <Box className="content">
-              <Text className="content__title">Mã số thuế </Text>
+            <Box className='content'>
+              <Text className='content__title'>Mã số thuế </Text>
               <span>:</span>
               <Text>3803124</Text>
             </Box>
           </Box>
           <Box>
-            <Box className="content">
-              <Text className="content__title">Người Đại Diện </Text>
+            <Box className='content'>
+              <Text className='content__title'>Người Đại Diện </Text>
               <span>:</span>
               <Text>Nguyễn Hoàng Khang</Text>
             </Box>
-            <Box className="content">
-              <Text className="content__title">Số Điện Thoại </Text>
+            <Box className='content'>
+              <Text className='content__title'>Số Điện Thoại </Text>
               <span>:</span>
               <Text>085xxxxxxx</Text>
             </Box>
-            <Box className="content">
-              <Text className="content__title">Chức Vụ </Text>
+            <Box className='content'>
+              <Text className='content__title'>Chức Vụ </Text>
               <span>:</span>
               <Text>Tổng Giám Đốc</Text>
             </Box>
-            <Box className="content">
-              <Text className="content__title">Số CMND </Text>
+            <Box className='content'>
+              <Text className='content__title'>Số CMND </Text>
               <span>:</span>
               <Text>044200001537</Text>
             </Box>
           </Box>
           <Box>
-            <Box className="content">
-              <Text className="content__title">Fax </Text>
+            <Box className='content'>
+              <Text className='content__title'>Fax </Text>
               <span>:</span>
               <Text>10000000</Text>
             </Box>
-            <Box className="content">
-              <Text className="content__title">Tên giao dịch quốc tế </Text>
+            <Box className='content'>
+              <Text className='content__title'>Tên giao dịch quốc tế </Text>
               <span>:</span>
               <Text>ABC</Text>
             </Box>
-            <Box className="content">
-              <Text className="content__title">Giấy chứng nhận số </Text>
+            <Box className='content'>
+              <Text className='content__title'>Giấy chứng nhận số </Text>
               <span>:</span>
               <Text>ABC</Text>
             </Box>
-            <Box className="content">
+            <Box className='content'>
               <Button
-                className="button__view"
+                className='button__view'
                 leftIcon={
-                  <AiOutlineFileSearch fontSize={"24px"} color="white" />
+                  <AiOutlineFileSearch fontSize={'24px'} color='white' />
                 }
               >
                 Giấy phép kinh doanh
@@ -92,27 +92,27 @@ const CompanyList = () => {
           </Box>
         </Box>
       </Box>
-      <Box className="box__body-right">
+      <Box className='box__body-right'>
         <Button
-          className="button__accept"
+          className='button__accept'
           leftIcon={
             <AiFillCheckCircle
-              color="white"
-              fontSize={"20px"}
-              fontWeight="700"
-              onClick={"console.log()"}
+              color='white'
+              fontSize={'20px'}
+              fontWeight='700'
+              onClick={'console.log()'}
             />
           }
         >
           Đồng ý
         </Button>
         <Button
-          className="button__refuse"
+          className='button__refuse'
           leftIcon={
             <AiFillCloseCircle
-              color="white"
-              fontSize={"20px"}
-              fontWeight="700"
+              color='white'
+              fontSize={'20px'}
+              fontWeight='700'
             />
           }
         >
@@ -125,9 +125,9 @@ const CompanyList = () => {
 
 function AdminPage(props) {
   return (
-    <Box bg={"#ccc"} w="100%" height="1400px">
-      <Box className="box__container">
-        <Text className="container__header">
+    <Box bg={'#ccc'} w='100%' height='1400px'>
+      <Box className='box__container'>
+        <Text className='container__header'>
           Danh Sách Doanh Nghiệp Đăng Ký
         </Text>
         <CompanyList />
