90eeae98ef4964b84a178a96f13b03225ebaf88d
noobmdev <noobmdev@gmail.com>
2022-04-27 14:30:01 +0700

    update deployed address

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,7 +2,7 @@ export const RPC_CHAIN_ID = 97;
 
 export const RPC_NODE_1 = 'https://data-seed-prebsc-1-s1.binance.org:8545/';
 
-export const MEDICAL_FACTORY_ADDRESS = '0x6F33085E638524a934Ef323adAF6028E3F8Af618';
+export const MEDICAL_FACTORY_ADDRESS = '0x4038b719C08497D090A831ddc30CB154A5539700';
 
 export const MEDICAL_FACTORY_METHODS = {
   agentInfos: 'agentInfos',
